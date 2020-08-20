82471e3c04630706b4bdf7323db2d54e1b08ba5f
Saif Khan <khansaifmohd93@gmail.com>
2020-08-20 12:10:22 +0200

    Addding predictions1

--- TensorBert/Prediction.py
+++ TensorBert/Prediction.py
@@ -10,7 +10,7 @@ bert_model_name="uncased_L-12_H-768_A-12"
 bert_ckpt_dir = os.path.join("model/", bert_model_name)
 tokenizer = FullTokenizer(vocab_file=os.path.join(bert_ckpt_dir, "vocab.txt"))
 print('Loading the model')
-model= load_model('part10000.h5', custom_objects={'BertModelLayer': BertModelLayer})
+model= load_model('saved_models/part10000.h5', custom_objects={'BertModelLayer': BertModelLayer})
 print('Model is loaded')
 print(model.summary())
 test=pd.read_csv('articles-validation-bypublisher.csv')
