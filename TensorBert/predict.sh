398593952fd472f9f0d717253ee2da9461c5842d
Saif Khan <khansaifmohd93@gmail.com>
2020-08-26 20:54:12 +0200

    addede a scrip for tira check2

--- TensorBert/predict.sh
+++ TensorBert/predict.sh
@@ -5,8 +5,8 @@ echo "Starting the prediction"
 INPUTDIR=${1?Error no input given}
 OUTDIR=${2?Error no output given}
 
-echo "conda activate projenv"
-conda activate projenv
+echo "/home/zenith-kaju/miniconda3/condabin/conda activate projenv"
+/home/zenith-kaju/miniconda3/condabin/conda activate projenv
 
 echo "python /home/zenith-kaju/NLP---Hyperpartisan-News-Detection/TensorBert/semeval-pan-2019-random-baseline.py --inputDataset=$INPUTDIR --outputDir=$OUTDIR"
 python /home/zenith-kaju/NLP---Hyperpartisan-News-Detection/TensorBert/semeval-pan-2019-random-baseline.py --inputDataset=$INPUTDIR --outputDir=$OUTDIR
