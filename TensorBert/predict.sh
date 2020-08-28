0a07ca31fd56c430ee8e662489e3c6651953cc74
Saif Khan <khansaifmohd93@gmail.com>
2020-08-29 01:47:56 +0200

    addede a scrip for tira check8

--- TensorBert/predict.sh
+++ TensorBert/predict.sh
@@ -6,12 +6,13 @@ INPUTDIR=${1?Error no input given}
 OUTDIR=${2?Error no output given}
 
 echo "/home/zenith-kaju/miniconda3/condabin/conda init bash"
-/home/zenith-kaju/miniconda3/condabin/conda init bash
+#/home/zenith-kaju/miniconda3/condabin/conda init bash
 
-source /home/zenith-kaju/.bashrc
+#source /home/zenith-kaju/.bashrc
 
 echo "/home/zenith-kaju/miniconda3/condabin/conda activate projenv"
-/home/zenith-kaju/miniconda3/condabin/conda activate projenv
+#/home/zenith-kaju/miniconda3/condabin/conda activate projenv
 
 echo "python /home/zenith-kaju/NLP---Hyperpartisan-News-Detection/TensorBert/semeval-pan-2019-random-baseline.py --inputDataset=$INPUTDIR --outputDir=$OUTDIR"
-python /home/zenith-kaju/NLP---Hyperpartisan-News-Detection/TensorBert/semeval-pan-2019-random-baseline.py --inputDataset=$INPUTDIR --outputDir=$OUTDIR
+#python /home/zenith-kaju/NLP---Hyperpartisan-News-Detection/TensorBert/semeval-pan-2019-random-baseline.py --inputDataset=$INPUTDIR --outputDir=$OUTDIR
+/home/zenith-kaju/miniconda3/envs/projenv/bin/python3.6 /home/zenith-kaju/NLP---Hyperpartisan-News-Detection/TensorBert/semeval-pan-2019-random-baseline.py --inputDataset=$INPUTDIR --outputDir=$OUTDIR
